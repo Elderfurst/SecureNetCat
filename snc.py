d5fd035c07a439dbdc300cb07516b05044157877
Nick Anderson <njcanderson@gmail.com>
2018-09-16 14:51:47 -0400

    starting to work on bi-directional

--- snc.py
+++ snc.py
@@ -9,16 +9,33 @@ parser = argparse.ArgumentParser(description='Secure Netcat')
 
 parser.add_argument('-l', action='store_true')
 parser.add_argument('--key')
-parser.add_argument('destination')
+parser.add_argument('destination', nargs='?')
 parser.add_argument('port')
 
 args = parser.parse_args()
+
+# Create the connection object
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+if bool(args.l):
+    connection.bind(('0.0.0.0', int(args.port)))
+    connection.listen(1)
+    clientSocket, clientAddress = connection.accept()
+
+    keepAlive = True
+
+    while keepAlive:
+        if clientSocket:
+            print('test print')
+        else:
+            # The client is no longer connected
+            keepAlive = False
+
 
 # Read data from stdin
 content = sys.stdin.read()
 
 # Connect to the specified server and send the data
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection.connect((args.destination, int(args.port)))
 connection.sendall(content)
 connection.shutdown(socket.SHUT_WR)
