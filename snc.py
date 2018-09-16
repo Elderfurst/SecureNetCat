f5fbed789546fd437ae8b79f3fd873d4e02fd6f6
Nick Anderson <njcanderson@gmail.com>
2018-09-16 14:25:15 -0400

    added beginnings of reading stdin and connecting

--- snc.py
+++ snc.py
@@ -1,10 +1,11 @@
 # Written in Python 3.7.0
-
 import argparse
+import sys
+import socket
+import select
 
 # Allow for the parsing of the correct command line arguments
-
-parser = argparse.ArgumentParser(description='Secure NetCat')
+parser = argparse.ArgumentParser(description='Secure Netcat')
 
 parser.add_argument('-l', action='store_true')
 parser.add_argument('--key')
@@ -13,4 +14,24 @@ parser.add_argument('port')
 
 args = parser.parse_args()
 
-print(args)
+# Read data from stdin
+content = sys.stdin.read()
+
+# Connect to the specified server and send the data
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection.connect((args.destination, int(args.port)))
+connection.sendall(content)
+connection.shutdown(socket.SHUT_WR)
+
+# Read the data coming in if established as a listener
+if bool(args.l):
+    incoming = ''
+    while True:
+        newData = connection.recv(4096)
+        if incoming == '':
+            break
+        else:
+            incoming += newData
+
+connection.close()
+print(incoming)
